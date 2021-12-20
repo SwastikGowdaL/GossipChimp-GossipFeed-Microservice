e225c52c4458be96be0dcf68620c2356504bce6d
swastik <swastik7999@gmail.com>
2021-12-20 14:26:57 +0530

    :sparkles: add function in client for connecting through socket to gossipInteraction microservice

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-undef
+const socket2 = io.connect('http://127.0.0.1:4000');
 const socket = io();
+
 const displayPosts = document.getElementById('posts');
 
 if (localStorage.getItem('post1')) {
@@ -20,4 +22,13 @@ socket.on('posts', (posts) => {
     p.style.border = '1px solid black';
     displayPosts.appendChild(p);
   }
+});
+
+socket2.emit('retrieveUserDetails', '617fc7e5e8bee9ff94617ab1');
+socket2.on('userDetails', (userDetails) => {
+  const p = document.createElement('p');
+  p.innerText = JSON.stringify(userDetails);
+  p.style.border = '3px solid blue';
+  displayPosts.prepend(p);
+  console.log(userDetails);
 });
